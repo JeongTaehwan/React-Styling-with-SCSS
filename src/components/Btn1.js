ec18828329f8d77aca62aae449787c317f2ffa15
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-04-26 20:12:28 +0900

    [ADD] Button
    [ADD] Button

---
+++ src/components/Btn1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Btn1.scss';
+
+function Btn1({ children }) {
+    return (
+        <button className="Btn1">{children}</button>
+    );
+}
+
+export default Btn1;
