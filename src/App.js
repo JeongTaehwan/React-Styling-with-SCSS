9a8ed5123acfb817a54a9545b6fcdfc012ef3e30
Tapuwan <68889258+JeongTaehwan@users.noreply.github.com>
2021-04-28 20:37:36 +0900

    [ADD] color props
    [ADD] color props

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ function App() {
         <Btn1>BUTTON</Btn1>
         <Btn1 size="small">BUTTON</Btn1>
       </div>
+      <div className="buttons">
+        <Btn1 size="large" color="gray">
+          BUTTON
+      </Btn1>
+        <Btn1 color="gray">BUTTON</Btn1>
+        <Btn1 size="small" color="gray">
+          BUTTON
+      </Btn1>
+      </div>
+      <div className="buttons">
+        <Btn1 size="large" color="pink">
+          BUTTON
+      </Btn1>
+        <Btn1 color="pink">BUTTON</Btn1>
+        <Btn1 size="small" color="pink">
+          BUTTON
+      </Btn1>
+      </div>
     </div>
   );
 }
--- src/components/Btn1.js
+++ src/components/Btn1.js
@@ -2,14 +2,15 @@ import React from 'react';
 import className from 'classnames';
 import './Btn1.scss';
 
-function Btn1({ children, size }) {
+function Btn1({ children, size, color }) {
     return (
-        <button className={className('Button', size)}>{children}</button>
+        <button className={className('Button', size, color)}>{children}</button>
     );
 }
 
 Btn1.defaultProps = {
-    size: 'medium'
+    size: 'medium',
+    color: 'blue'
 };
 
 export default Btn1;
